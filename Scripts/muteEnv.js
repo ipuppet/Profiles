ac80f1bfe65b9fd828b41d16f4dd4c8c55f354f9
ipuppet <zimingw.ol@outlook.com>
2023-08-17 21:04:45 +0800

    update

--- Scripts/muteEnv.js
+++ Scripts/muteEnv.js
@@ -2,9 +2,9 @@
 
 let body = $response.body
 
-body = body.replaceAll("this.log", "__mute")
+body = body.replaceAll("this.log(", "__mute(") // `(` 防止误伤其他方法
 body = body.replaceAll("$.log", "__mute")
-body = body.replaceAll("this.msg", "__mute")
+body = body.replaceAll("this.msg(", "__mute(")
 body = body.replaceAll("$.msg", "__mute")
 body = "const __mute=()=>{};\n" + body
 
